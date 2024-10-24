27a26ceeb35d3f0c8da7925133628f6a380ec07a
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-10-24 18:15:03 +0200

    change api

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { reactive } from "vue";
 export const store = reactive({
-  apiUrl:'http://127.0.0.1:8000/api/',
+  apiUrl:'http://laravel-api-production-a3ce.up.railway.app/api/',
   projects:[],
   technologies:[],
   types:[],
