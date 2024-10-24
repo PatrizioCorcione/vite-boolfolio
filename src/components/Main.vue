9159ccba6805fa377aae32387c1d97653feac13d
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-10-24 18:39:33 +0200

    test

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,7 @@
 <script>
 
 import axios from 'axios';
-import {store} from '../store';
+import { store } from '../store';
 import ProjectFilter from '../components/subComponents/ProjectFilter.vue';
 import ProjectList from './subComponents/ProjectList.vue';
 import TopProject from '../components/subComponents/TopProject.vue';
@@ -12,81 +12,73 @@ import Hero from '../components/subComponents/Hero.vue';
 import TechnologyIcons from './subComponents/TechnologyIcons.vue';
 import ArrowToTop from './subComponents/ArrowToTop.vue';
 
-
-
-
 export default {
-  components:{
-      ProjectList,
-      ProjectFilter,
-      Loader,
-      TopProject,
-      AboutMe,
-      Contact,
-      Hero,
-      TechnologyIcons,
-      ArrowToTop
-    },
+  components: {
+    ProjectList,
+    ProjectFilter,
+    Loader,
+    TopProject,
+    AboutMe,
+    Contact,
+    Hero,
+    TechnologyIcons,
+    ArrowToTop
+  },
   data() {
     return {
       store,
-      itsReady:false
-      
+      itsReady: false
     }
   },
   methods: {
-    getApi(type = 'project'){
-      axios.get(this.store.apiUrl + type )
-      
-      .then(result =>{
-        this.itsReady = true
-        switch (type) {
-          case 'technology':
-          this.store.technologies = result.data;
-
-            break;
-          case 'type':
-          this.store.types = result.data;
-
-            break;
-        
-          default:
-          this.store.projects = result.data;
-
-            break;
-        }
-      })
+    getApi(type = 'project') {
+      return axios.get(this.store.apiUrl + type)
+        .then(result => {
+          switch (type) {
+            case 'technology':
+              this.store.technologies = result.data;
+              break;
+            case 'type':
+              this.store.types = result.data;
+              break;
+            default:
+              this.store.projects = result.data;
+              break;
+          }
+        });
     },
   },
   mounted() {
-    this.getApi();
-    this.getApi('type');
-    this.getApi('technology');
-    window.scrollTo(0, 0);
+    this.getApi()          // Prima chiamata
+      .then(() => this.getApi('type'))  // Seconda chiamata
+      .then(() => this.getApi('technology')) // Terza chiamata
+      .then(() => {
+        this.itsReady = true; // Imposta itsReady solo dopo che tutte le chiamate sono completate
+        window.scrollTo(0, 0);
+      })
+      .catch(error => {
+        console.error("Errore durante il caricamento dei dati:", error); // Gestione degli errori
+      });
   },
 }
 
 </script>
 
 <template>
-<Loader v-if="!this.itsReady" />
-<div v-else class="main">
-
-  <Hero/>
-  <AboutMe/>
-  <TechnologyIcons/>
-  <TopProject/>
-  <ProjectList/>
-  <Contact/>
-  <ArrowToTop/>
-
-</div>
-  
+  <Loader v-if="!itsReady" />
+  <div v-else class="main">
+    <Hero />
+    <AboutMe />
+    <TechnologyIcons />
+    <TopProject />
+    <ProjectList />
+    <Contact />
+    <ArrowToTop />
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .main {
   background: linear-gradient(135deg, rgba(0, 0, 0, 0.9), rgba(22, 33, 69, 0.8), rgba(30, 30, 30, 0.7), rgba(65, 105, 225, 0.5));
 }
-
 </style>
