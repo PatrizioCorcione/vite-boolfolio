b84766ae44ce2fff81db105e953d3e1b4376e846
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-10-24 18:20:04 +0200

    https

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { reactive } from "vue";
 export const store = reactive({
-  apiUrl:'http://laravel-api-production-a3ce.up.railway.app/api/',
+  apiUrl:'https://laravel-api-production-a3ce.up.railway.app/api/',
   projects:[],
   technologies:[],
   types:[],
